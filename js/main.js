0da6df63b50c20ccd900ea7f1f1a321b0dbd5e41
rahul gupta <rahulitsmca@gmail.com>
2013-05-24 17:29:49 +0530

    step4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 var app = {
+
+
+    renderHomeView: function () {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -23,9 +35,8 @@ var app = {
     initialize: function () {
         var self = this;
         this.store = new LocalStorageStore(function () {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
