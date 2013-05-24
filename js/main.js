e0c2d57285f76e9c3057de5acd1b6e2757c13364
rahul gupta <rahulitsmca@gmail.com>
2013-05-24 17:04:22 +0530

    step 3

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
 var app = {
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
-    findByName: function() {
+    findByName: function () {
         console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
+        this.store.findByName($('.search-key').val(), function (employees) {
             var l = employees.length;
             var e;
             $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
+            for (var i = 0; i < l; i++) {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
     },
 
-    initialize: function() {
-        this.store = new LocalStorageStore();
+    initialize: function () {
+        var self = this;
+        this.store = new LocalStorageStore(function () {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
+
+
+
 
 app.initialize();
